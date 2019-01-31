16040053a409ce36903adf901bd7abe308324338
Stefan <oyetorostephven@gmail.com>
2019-01-31 18:27:02 +0100

    Initial commit

---
+++ main.js
@@ -0,0 +1,50 @@
+console.log("main process running");
+
+const electron = require('electron')
+
+// Module to control application life.
+const app = electron.app
+
+// Module to create native browser window.
+const BrowserWindow = electron.BrowserWindow
+
+const path = require('path')
+const url = require('url')
+
+// Keep a global reference of the window object, if you don't, the window will
+// be closed automatically when the JavaScript object is garbage collected.
+let mainWindow
+
+function createWindow() {
+    // Create the browser window.
+    mainWindow = new BrowserWindow();
+
+    mainWindow.loadURL(url.format({
+        pathname: path.join(__dirname, 'index.html'),
+        protocol: 'file',
+        slashes: true
+    }));
+
+    mainWindow.on('closed', () => {
+        mainWindow = null;
+    })
+}
+
+app.on("ready", createWindow);
+
+// Quit when all windows are closed.
+app.on('window-all-closed', () => {
+    // On OS X it is common for applications and their menu bar
+    // to stay active until the user quits explicitly with Cmd + Q
+    if (process.platform !== 'darwin') {
+        app.quit()
+    }
+})
+
+app.on('activate', () => {
+    // On OS X it's common to re-create a window in the app when the
+    // dock icon is clicked and there are no other windows open.
+    if (mainWindow === null) {
+        createWindow()
+    }
+})
